51f6727974f77458ca0ed0afda49e86ef4dabc0b
maxy-coder <maxy50646@gmail.com>
2024-12-20 12:20:19 +0530

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+let p = document.createElement("h1");
+function register(){
+    p.innerText = "Welcome to the Register page";
+    document.body.appendChild(p);
+    p.style.textAlign = "center";
+}
